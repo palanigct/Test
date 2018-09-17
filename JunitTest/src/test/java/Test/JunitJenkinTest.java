94cc869e261662240d312a2d96bf7af077b553b7
Palanisamy <palanisamygct.instro@gmail.com>
2018-09-18 04:27:11 +0530

    commit

---
+++ JunitTest/src/test/java/Test/JunitJenkinTest.java
@@ -0,0 +1,27 @@
+package Test;
+
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JunitJenkinTest {
+	
+	@Test
+	public void testpass() {
+		int a=1;
+		int b=2;
+		assertEquals(a+b, 3);
+	}
+	
+	
+
+	@Test
+	public void testfail() {
+		int a=1;
+		int b=2;
+		assertEquals(a+b, 3);
+	}
+
+}

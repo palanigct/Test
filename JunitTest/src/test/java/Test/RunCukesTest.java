a8335a7e1e64846991d947fd69e58656a017105a
Palanisamy <palanisamygct.instro@gmail.com>
2018-09-20 01:32:51 +0530

    Added cucumber report dependencies and plugins

--- JunitTest/src/test/java/Test/RunCukesTest.java
+++ JunitTest/src/test/java/Test/RunCukesTest.java
@@ -6,7 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/features",glue = {"stepDeff"},tags= {"@test1"})
+@CucumberOptions(features="src/test/resources/features",glue = {"stepDeff"},plugin = { "pretty", "html:target/Reports/cucumber-html-report", "json:target/Reports/cucumber.json",
+"junit:target/Reports/cucumber.xml" },tags= {"@test1"})
 public class RunCukesTest {
 
 }
